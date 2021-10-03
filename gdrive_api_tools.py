8b8852d231b656d973eb1f08e7c9dac6b043443f
ahak0 <a_hak0@yahoo.com>
2021-10-03 00:58:45 -0700

    adhering to google's python code style guidelines

--- gdrive_api_tools.py
+++ gdrive_api_tools.py
@@ -11,25 +11,23 @@ CLIENT_SECRET = 'client_secret.json'
 HTTP_OK = 200
 
 def get_authorization() -> discovery.Resource:
-   """ Executes the OAuth2Client flow. 
-   
-   Returns:
-      A googleapiclient.discovery.Resource object that has authorization to be
-      accessed.
-   """
-   store = file.Storage('storage.json')
-   creds = store.get()
+    """ Executes the OAuth2Client flow. 
 
-   if not creds or creds.invalid:
-      oauth_flow = client.flow_from_clientsecrets(CLIENT_SECRET, SCOPES)
-      creds = tools.run_flow(oauth_flow, store)
-
-   DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
-   return DRIVE
+        Returns:
+            A googleapiclient.discovery.Resource object that has authorization
+            to be accessed.
+    """
+    store = file.Storage('storage.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        oauth_flow = client.flow_from_clientsecrets(CLIENT_SECRET, SCOPES)
+        creds = tools.run_flow(oauth_flow, store)
+    DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+    return DRIVE
 
 def upload_file(drive_service, filename) -> None:
-   """ Uploads the file specified to the service provided. """
-   metadata = {'name': filename}
-   result = drive_service.files().create(body=metadata, media_body=filename).execute()
-   if result:
-      print('Successfully uploaded "%s" (%s)' % (filename, result['mimeType']))
+    """ Uploads the file specified to the service provided. """
+    metadata = {'name': filename}
+    result = drive_service.files().create(body=metadata, media_body=filename).execute()
+    if result:
+        print('Successfully uploaded "%s" (%s)' % (filename, result['mimeType']))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from gdrive_api_tools import get_authorization, upload_file
 
 def main():
-   drive = get_authorization()
-   upload_file(drive, 'acc nerves.png')
-   drive.close()
+    drive = get_authorization()
+    upload_file(drive, 'acc nerves.png')
+    drive.close()
 if __name__ == '__main__':
-   main()
+    main()
